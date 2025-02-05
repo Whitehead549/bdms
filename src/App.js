fa3f533cff07378188c63a01d5ec1b9be17892e9
Whitehead549 <finbareric549@gmail.com>
2025-02-05 09:50:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ import ContactUs from './pages/ContactUs';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Welcome from './pages/Welcome';
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Import Font Awesome
 
 function App() {
   return (
-    <div className="bg-[#000300] overflow-x-hidden">
+    <div className="bg-[#000300] overflow-x-hidden relative">
       <Router>
         <Content />
       </Router>
+
+      {/* Telegram Chat Icon with Label */}
+      <a
+        href="https://t.me/msverovixen"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed bottom-6 right-6 bg-blue-500 hover:bg-blue-700 text-white p-3 rounded-full shadow-lg transition duration-300 ease-in-out z-50 pointer-events-auto flex items-center space-x-2"
+      >
+        <i className="fab fa-telegram-plane text-2xl"></i>
+        <span className="text-sm font-semibold">Chat Me</span>
+      </a>
     </div>
   );
 }
